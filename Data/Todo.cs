21d5d07968b9390622759b1241997f77d4aa3498
Rodolphe <rodolphe.jamin@gmail.com>
2022-09-28 17:24:11 +0200

    Story 1 : List my TODOs

---
+++ Data/Todo.cs
@@ -0,0 +1,25 @@
+﻿namespace Todolist.Data
+{
+    public class Todo
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (string.IsNullOrEmpty(value)) { throw new ArgumentNullException("Valeur incorrecte pour le titre."); }
+                _title = value;
+            }
+        }
+        public string Description { get; set; }
+
+
+
+        public Todo(string title, string description = "")
+        {
+            Title = title;
+            Description = description;
+        }
+    }
+}
---
+++ Data/TodoService.cs
@@ -0,0 +1,23 @@
+﻿namespace Todolist.Data
+{
+    public class TodoService
+    {
+
+#if DEBUG
+        private List<Todo> _todoList = new List<Todo>()
+        {
+            new Todo("Faire les courses", "Acheter du pain de mie, du fromage et du jambon"),
+            new Todo("Preparer les croques-monsieurs", "Cuisson 10 min a 180°"),
+            new Todo("Se régaler")
+        };
+#else
+
+        private List<Todo> _todoList = new List<Todo>();
+#endif
+
+        public async Task<Todo[]> GetTodoAsync()
+        {
+            return _todoList.ToArray();
+        }
+    }
+}
